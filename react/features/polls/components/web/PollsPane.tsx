6fa73cde9d5aa9778c35d02b60bd5993ea1a482e
haseebthedev <haseebhowto@gmail.com>
2025-02-03 17:53:32 +0500

    added poll disable functionality

--- react/features/polls/components/web/PollsPane.tsx
+++ react/features/polls/components/web/PollsPane.tsx
@@ -1,50 +1,62 @@
-import React from 'react';
-import { makeStyles } from 'tss-react/mui';
+import React from "react";
+import { makeStyles } from "tss-react/mui";
 
-import Button from '../../../base/ui/components/web/Button';
-import AbstractPollsPane, { AbstractProps } from '../AbstractPollsPane';
+import Button from "../../../base/ui/components/web/Button";
+import AbstractPollsPane, { AbstractProps } from "../AbstractPollsPane";
 
-import PollCreate from './PollCreate';
-import PollsList from './PollsList';
+import PollCreate from "./PollCreate";
+import PollsList from "./PollsList";
+import { IReduxState } from "../../../app/types";
+import { useSelector } from "react-redux";
+import { isLocalParticipantModerator } from "../../../base/participants/functions";
 /* eslint-enable lines-around-comment */
 
 const useStyles = makeStyles()(() => {
     return {
         container: {
-            height: '100%',
-            position: 'relative'
+            height: "100%",
+            position: "relative",
         },
         listContainer: {
-            height: 'calc(100% - 88px)',
-            overflowY: 'auto'
+            height: "calc(100% - 88px)",
+            overflowY: "auto",
         },
         footer: {
-            position: 'absolute',
+            position: "absolute",
             bottom: 0,
-            padding: '24px',
-            width: '100%',
-            boxSizing: 'border-box'
-        }
+            padding: "24px",
+            width: "100%",
+            boxSizing: "border-box",
+        },
     };
 });
 
 const PollsPane = ({ createMode, onCreate, setCreateMode, t }: AbstractProps) => {
     const { classes } = useStyles();
 
-    return createMode
-        ? <PollCreate setCreateMode = { setCreateMode } />
-        : <div className = { classes.container }>
-            <div className = { classes.listContainer } >
-                <PollsList setCreateMode = { setCreateMode } />
+    const state = useSelector((state: IReduxState) => state);
+
+    const iamModerator = isLocalParticipantModerator(state);
+
+    return createMode ? (
+        <PollCreate setCreateMode={setCreateMode} />
+    ) : (
+        <div className={classes.container}>
+            <div className={classes.listContainer}>
+                <PollsList setCreateMode={setCreateMode} />
             </div>
-            <div className = { classes.footer }>
-                <Button
-                    accessibilityLabel = { t('polls.create.create') }
-                    fullWidth = { true }
-                    labelKey = { 'polls.create.create' }
-                    onClick = { onCreate } />
-            </div>
-        </div>;
+            {iamModerator && (
+                <div className={classes.footer}>
+                    <Button
+                        accessibilityLabel={t("polls.create.create")}
+                        fullWidth={true}
+                        labelKey={"polls.create.create"}
+                        onClick={onCreate}
+                    />
+                </div>
+            )}
+        </div>
+    );
 };
 
 /*
